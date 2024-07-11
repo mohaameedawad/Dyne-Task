0d77c14c6c786f46d6ec97bdcd27dfd8af8c98cb
mohaameedawad <mohammedawaad26@gmail.com>
2024-07-11 20:36:53 +0300

    creating menu branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'menu', loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
